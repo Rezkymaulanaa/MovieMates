940eb2e2ea13ffdba38a762dae5c3aa2f02eb16f
maykoRa <maykoraditya34@gmail.com>
2023-05-31 14:25:47 +0800

    feat: creating a navbar in homescene

--- app/src/main/java/projectfinal/Scene/HomeScene.java
+++ app/src/main/java/projectfinal/Scene/HomeScene.java
@@ -1,7 +1,10 @@
 package projectfinal.Scene;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class HomeScene {
@@ -13,7 +16,19 @@ public class HomeScene {
 
     public void show() {
 
-        Scene scene = new Scene(new VBox(), 600, 700);
+        Button home = new Button("Home");
+        Button genre = new Button("Genre");
+
+        home.setOnAction(e -> System.out.println("Home button clicked"));
+        genre.setOnAction(e -> System.out.println("Genre button clicked"));
+        
+        HBox navbar = new HBox();
+        navbar.setSpacing(10);
+        navbar.setPadding(new Insets(10));
+        navbar.getChildren().addAll(home, genre);
+        navbar.setAlignment(Pos.TOP_CENTER);
+
+        Scene scene = new Scene(navbar , 600, 700);
         stage.setScene(scene);
         stage.show();
 
