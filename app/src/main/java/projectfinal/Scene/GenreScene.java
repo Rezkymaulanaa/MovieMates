8b5195f5170a04d66fd114c1171e71d952db4ccc
NaaoiCode <muhnabil7604@gmail.com>
2023-06-01 09:52:27 +0800

    added a new scene named GenreScene and connecting it with HomeScene

--- app/src/main/java/projectfinal/Scene/GenreScene.java
+++ app/src/main/java/projectfinal/Scene/GenreScene.java
@@ -4,40 +4,36 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class HomeScene {
+public class GenreScene {
     private Stage stage;
     private String nameValue;
 
-    public HomeScene(Stage stage, String nameValue) {
+    public GenreScene(Stage stage, String nameValue) {
         this.stage = stage;
         this.nameValue = nameValue;
     }
 
     public void show() {
-        Label nameLabel = new Label("Hi, " + nameValue);
-        nameLabel.setAlignment(Pos.CENTER);
-
         Button home = new Button("Home");
         Button genre = new Button("Genre");
 
-        home.setOnAction(e -> System.out.println("Home button clicked"));
-        genre.setOnAction(e -> {
-            System.out.println("Genre button clicked");
-            GenreScene genreScene = new GenreScene(stage, nameValue);
-            genreScene.show();
+        home.setOnAction(e -> {
+            System.out.println("Home button clicked");
+            HomeScene homeScene = new HomeScene(stage, nameValue);
+            homeScene.show();
         });
+        genre.setOnAction(e -> System.out.println("Genre button clicked"));
 
         HBox navbar = new HBox(home, genre);
         navbar.setSpacing(10);
         navbar.setAlignment(Pos.CENTER_LEFT);
 
-        VBox content = new VBox(navbar, nameLabel);
+        VBox content = new VBox(navbar);
         content.setAlignment(Pos.TOP_LEFT);
         content.setSpacing(10);
         content.setPadding(new Insets(10));
