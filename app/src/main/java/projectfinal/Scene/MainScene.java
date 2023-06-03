870d346a330615cd4c54c94c3bbb6fdc6e305e09
maykoRa <maykoraditya34@gmail.com>
2023-06-03 15:55:22 +0800

    feat: adding background image to mainscene

--- app/src/main/java/projectfinal/Scene/MainScene.java
+++ app/src/main/java/projectfinal/Scene/MainScene.java
@@ -4,7 +4,10 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -16,6 +19,8 @@ public class MainScene {
     }
 
     public void show() {
+        StackPane img = new StackPane();
+        
         Label title = new Label("Hello MovieMates");
         Label name = new Label("Set your name");
 
@@ -24,6 +29,11 @@ public class MainScene {
             HomeScene homeScene = new HomeScene(stage);
             homeScene.show();
         });
+
+
+        Image backImage = new Image(getClass().getClassLoader().getResourceAsStream("images/Background_Mainscene.jpg"));
+        ImageView iView = new ImageView(backImage);
+        img.getChildren().add(iView);
 
         HBox apptitle = new HBox();
         apptitle.getChildren().add(title);
@@ -37,8 +47,9 @@ public class MainScene {
         VBox layout = new VBox();
         layout.getChildren().addAll(apptitle, content);
         layout.setAlignment(Pos.CENTER_LEFT);
+        img.getChildren().addAll(layout);
 
-        Scene scene = new Scene(layout, 960, 540);
+        Scene scene = new Scene(img, 960, 540);
         stage.setScene(scene);
         stage.show();
     }
